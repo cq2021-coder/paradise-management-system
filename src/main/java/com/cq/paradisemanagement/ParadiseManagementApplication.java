ab1833274793224023ef5128714a29dbe061538e
cq2021 <2972084238@qq.com>
2021-09-11 19:21:49 +0800

    基础框架

---
+++ src/main/java/com/cq/paradisemanagement/ParadiseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cq.paradisemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadiseManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParadiseManagementApplication.class, args);
+    }
+
+}
